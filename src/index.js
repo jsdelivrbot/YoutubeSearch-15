1178540245abc652651fb971563a10320157f09a
otsybulsky <helg37@gmail.com>
2018-03-24 10:40:55 +0200

    refactoring App to class, add state

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
-
 import API_KEY from './api_key'
 // create and add api_key.js is .gitignore
 //  const API_KEY = "your youtube api key"
 //  export default API_KEY
 
-YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data))
+class App extends Component {
+    constructor(props) {
+        super(props)
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    ) 
+        this.state = { videos: [] }
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })
+            //this.setState({ videos: videos }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
