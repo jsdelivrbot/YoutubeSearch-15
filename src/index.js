99ba7bbb993af14ffec70b93e375a90b4d2856ae
otsybulsky <helg37@gmail.com>
2018-03-24 10:26:12 +0200

    fix api_key.js format, test youtube search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar'
 
 import API_KEY from './api_key'
 // create and add api_key.js is .gitignore
-//  export const API_KEY = "your youtube api key"
+//  const API_KEY = "your youtube api key"
+//  export default API_KEY
+
+YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data))
 
 const App = () => {
     return (
