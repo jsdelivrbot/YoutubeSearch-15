fa5571457232dd6baf6f60d1f6cb723fcbf55056
otsybulsky <helg37@gmail.com>
2018-03-24 09:27:08 +0200

    event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react' // const Component = React.Component
 
 class SearchBar extends Component {
+    // render() {
+    //     return <input onChange={this.onInputChange} />
+    // }
+
+    // onInputChange(event) { //handler, name format onHandlerName
+    //     console.log(event.target.value)                
+    // }
+
     render() {
-        return <input />
+        return <input onChange={(event) => console.log(event.target.value)} />
     }
 }
 
