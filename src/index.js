ac30c83c0270c5e93d4be0310849f7bfbee81e78
otsybulsky <helg37@gmail.com>
2018-03-24 08:02:54 +0200

    youtube api key console.developers.google.com, npm i -S youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import API_KEY from './api_key'
+// create and add api_key.js is .gitignore
+//  export const API_KEY = "your youtube api key"
 
 const App = () => {
     return <div>Hi!</div>
