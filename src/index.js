a72b2ba170da6413a9b470eaddf9e1e01e6b8ae6
otsybulsky <helg37@gmail.com>
2018-03-24 08:16:16 +0200

    add component SearchBar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+
 import API_KEY from './api_key'
 // create and add api_key.js is .gitignore
 //  export const API_KEY = "your youtube api key"
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    ) 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
